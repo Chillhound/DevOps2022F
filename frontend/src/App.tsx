bdc04fe7ddf77bd58ba7b8b6c19e440683099131
axrez <emil@filify.app>
2022-02-08 13:58:41 +0100

    Re-initialize with ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [counter, setCounter] = React.useState(0);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. It has been done
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,9 +18,6 @@ function App() {
         >
           Learn React
         </a>
-
-        <p>{counter}</p>
-        <button onClick={() => setCounter((x) => x + 1)}>Increment</button>
       </header>
     </div>
   );
