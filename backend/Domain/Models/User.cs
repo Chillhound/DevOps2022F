ce1ec443ccb1985269477c02ec23a8ea1aa0b20c
Mikkel-MJ <mikkel1260@gmail.com>
2022-02-12 14:47:44 +0100

    wip

--- backend/Domain/Models/User.cs
+++ backend/Domain/Models/User.cs
@@ -12,7 +12,7 @@ namespace Domain.Models
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
-        public ICollection<User> Followers { get; set; }
-        public ICollection<Message> Messages { get; set; }
+        public ICollection<User>? Followers { get; set; }
+        public ICollection<Message>? Messages { get; set; }
     }
 }
--- backend/MiniTwit-API/Controllers/UserController.cs
+++ backend/MiniTwit-API/Controllers/UserController.cs
@@ -1,9 +1,14 @@
 ﻿using DataAccess;
+using Domain.DTO;
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace MiniTwit_API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class UserController : Controller
     {
 
@@ -14,38 +19,91 @@ namespace MiniTwit_API.Controllers
             this.context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<User>> GetUserById(int id)
+        //[HttpGet]
+        //public async Task<ActionResult<User>> GetUserById(int id)
+        //{
+        //   return await context.Users.FindAsync(id);
+        //}
+        [HttpPost("Register")]
+        public IActionResult Post([FromBody] CreateUserDTO userDTO)
         {
-           return await context.Users.FindAsync(id);
+            if (userDTO == null) return BadRequest();
+            string hashed = GetHash(userDTO.Password);
+
+            User user = new User
+            {
+                Email = userDTO.Email,
+                UserName = userDTO.UserName,
+                PasswordHash = hashed
+            };
+
+
+            context.Add(user);
+            context.SaveChanges();
+
+            return NoContent();
+        }
+      
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginDTO login)
+        {
+            if (login.Email == null || login.Password == null) return BadRequest();
+
+            User user = context.Users.Where(x => x.Email == login.Email && x.PasswordHash == GetHash(login.Password) ).FirstOrDefault();
+
+            if(user == null) return BadRequest();
+
+
+          return Ok(login);
         }
 
-        [HttpGet]
+        [HttpGet("Timeline")]
         public ActionResult<List<Message>> GetMessageTimeline(int id, int limit)
         {
             User user = context.Users.Find(id);
 
             List<Message> messages = new List<Message>();
 
-            messages.AddRange(user.Messages.Where(x => x.Flagged == 0).ToList());
+            if(user.Messages != null) messages.AddRange(user.Messages.Where(x => x.Flagged == 0).ToList()) ;
 
-            foreach (var x in user.Followers)
-            {
-                var mes = x.Messages.Where(m => m.Flagged == 0).ToList();
+            if(user.Followers != null)
+            {  
+                foreach (var x in user.Followers)
+                {
+                    IEnumerable<Message> mes = new List<Message>();
 
-                messages.AddRange(mes);
+                    if (x.Messages != null) mes = x.Messages.Where(m => m.Flagged == 0).ToList();
+
+                    messages.AddRange(mes);
+                }
             }
 
             return messages.OrderByDescending(x => x.PubDate).Take(limit).ToList();
         }
 
-        [HttpGet]
-        public ActionResult<List<Message>> GetMessages(int id)
+        //[HttpGet]
+        //public ActionResult<List<Message>> GetMessages(int id)
+        //{
+
+        //    return context.Users.Find(id).Messages.ToList();
+        //}
+
+        private static string GetHash(string password)
         {
-           
-            return context.Users.Find(id).Messages.ToList();
+            byte[] salt = new byte[128 / 8];
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+            Console.WriteLine($"Hashed: {hashed}");
+            return hashed;
         }
-
-
     }
 }
