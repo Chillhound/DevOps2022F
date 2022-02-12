133ed018ed33a103c215c0811b667117893aa6c3
Mikkel-MJ <mikkel1260@gmail.com>
2022-02-12 16:26:42 +0100

    wip

--- backend/MiniTwit-API/Controllers/MessageController.cs
+++ backend/MiniTwit-API/Controllers/MessageController.cs
@@ -1,9 +1,12 @@
 ﻿using DataAccess;
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MiniTwit_API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class MessageController : Controller
     {
         MiniTwitContext context;
@@ -11,10 +14,32 @@ namespace MiniTwit_API.Controllers
         {
             this.context = context;
         }
-        [HttpGet]
+        [HttpGet ("PublicTimeline")]
         public ActionResult<List<Message>> PublicTimeline(int limit)
         {
             return context.Messages.OrderByDescending(x => x.PubDate).Take(limit).ToList();
+        }
+
+        [HttpPost]
+        public ActionResult<List<Message>> Post(string messageText)
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            int userId = int.Parse(identity.FindFirst("Id").Value);
+
+            Message message = new Message
+            {
+                PubDate = DateTime.Now,
+                Flagged = 0,
+                Author = context.Users.Find(userId),
+                AuthorId = userId,
+                Text = messageText
+            };
+
+            context.Messages.Add(message);
+            context.SaveChanges();
+
+            return Ok(message);
         }
     }
 }
