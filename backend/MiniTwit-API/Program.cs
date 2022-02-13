d49b6eb8dda3719ec70cbaa1cec6d93a252630e1
kaeppen <seho@itu.dk>
2022-02-13 22:18:26 +0100

    use sqlserver instead of sqlite

--- backend/MiniTwit-API/Program.cs
+++ backend/MiniTwit-API/Program.cs
@@ -13,8 +13,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<MiniTwitContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
-
+var connection = @"Server=db;Database=master;User=sa;Password=Belnis12456!;";
+//builder.Services.AddDbContext<MiniTwitContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
+builder.Services.AddDbContext<MiniTwitContext>(options => options.UseSqlServer(connection));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
