52bfe86b202e09467cad739a1b8dced33de5cca4
Mikkel-MJ <mikkel1260@gmail.com>
2022-02-13 21:08:14 +0100

    added isfollowing to userMessages

---
+++ backend/Domain/DTO/UserMessagesDTO.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class UserMessagesDTO
+    {
+        public bool IsFollowing { get; set; }
+        public List<Message> messages { get; set; }
+    }
+}
