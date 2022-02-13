6d6885588a3d6eb9723eeda59ccb24e56c91990c
kaeppen <seho@itu.dk>
2022-02-13 13:26:05 +0100

    Progress testing AccountController

--- backend/MiniTwit-API/ControllerTests/AccountControllerTests.cs
+++ backend/MiniTwit-API/ControllerTests/AccountControllerTests.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace ControllerTests;
 public class AccountControllerTests
 {
@@ -10,7 +12,66 @@ public class AccountControllerTests
         var builder = new DbContextOptionsBuilder<MiniTwitContext>();
         builder.UseSqlite(connection);
         var context = new MiniTwitContext(builder.Options);
+        context.Database.EnsureCreated();
         _controller = new AccountController(context);
     }
+
+    [Fact]
+    public void Register_given_valid_user_returns_ok_response()
+    {
+        var newUser = new CreateUserDTO
+        {
+            UserName = "mikkis_far",
+            Email = "test@test.dk",
+            Password = "benis"
+        };
+
+        var result = _controller.Post(newUser).Result;
+        var okResult = result as OkObjectResult;    
+        
+        //courtesy of: https://stackoverflow.com/questions/41292919/unit-testing-controller-methods-which-return-iactionresult 
+        Assert.NotNull(result);
+        Assert.Equal(200, okResult.StatusCode);        
+    }
+
+    [Fact]
+    public void Register_given_invalid_user_returns_badrequest()
+    {
+        CreateUserDTO newUser = null;
+
+        var result = _controller.Post(newUser).Result;
+        var badResult = result as BadRequestResult;
+
+        Assert.Equal(400, badResult.StatusCode);
+    }
+
+    [Fact]
+    public void Login_given_user_with_null_email_returns_badrequest()
+    {
+        var user = new LoginDTO
+        {
+            Email = null,
+            Password = "SikkertKodeord125"
+        };
+
+        var result = _controller.Login(user).Result;
+        var badResult = result as BadRequestObjectResult; 
+        Assert.Equal(400, badResult.StatusCode);
+    }
+
+    [Fact]
+    public void Login_given_user_with_full_credentials_returns_okresult()
+    {
+        var user = new LoginDTO
+        {
+            Email = "test@test.dk",
+            Password = "BiteThePillow184"
+        };
+
+        var result = _controller.Login(user).Result;
+        
+        //TODO needs seeding before we can progress
+    }
+
 }
 
--- backend/MiniTwit-API/ControllerTests/GlobalUsings.cs
+++ backend/MiniTwit-API/ControllerTests/GlobalUsings.cs
@@ -1,6 +1,8 @@
 global using Xunit;
 global using System;
 global using DataAccess;
+global using Domain.DTO;
 global using MiniTwit_API.Controllers;
 global using Microsoft.Data.Sqlite;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
