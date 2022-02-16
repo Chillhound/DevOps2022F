ca02ad42c6899ec0d031020e8d72bb558585ee36
kaeppen <seho@itu.dk>
2022-02-16 18:24:54 +0100

    Progress public api

--- backend/MiniTwit-Public-API/Controllers/PublicController.cs
+++ backend/MiniTwit-Public-API/Controllers/PublicController.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.DTO;
+using DataAccess;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Models;
 
 namespace MiniTwit_Public_API.Controllers
 {
@@ -14,13 +16,21 @@ namespace MiniTwit_Public_API.Controllers
     [ApiController]
     public class PublicController : ControllerBase
     {
+        private readonly MiniTwitContext _context;
+
+        public PublicController(MiniTwitContext context)
+        {
+            _context = context;
+        }
 
         int latest = 0;
 
         [HttpGet ("msgs")]
-        public IActionResult GetMessages()
+        public ActionResult<ICollection<Message>> GetMessages()
         {
-            return null;
+            //TODO: skal latest opdateres? I så fald: hvordan?
+            var messages = _context.Messages.Where(m => m.Flagged == 0).Select(m => m).ToList();
+            return messages;
         }
 
         [HttpGet("latest")]
@@ -29,18 +39,41 @@ namespace MiniTwit_Public_API.Controllers
             return new LatestResult { latest = latest };
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("msgs/{username}")]
         [Route("msgs/{username}")]
-        public IActionResult GetMessagesUser(string username)
+        public ActionResult<ICollection<Message>> GetMessagesUser(string username)
         {
-            return null;
+            //Er det muligt at denne skal returnere 404 i tilfælde af ikke-eksisterende bruger?
+            var user = _context.Users.Where(u => u.UserName == username).Select(u => u).FirstOrDefault();
+            var messages = _context.Messages.Where(m => m.UserId == user.UserId).Where(m => m.Flagged == 0).Select(m => m).ToList();
+            return messages; 
         }
 
-        [HttpPost("{username}")]
+        [HttpPost("msgs/{username}")]
         [Route("msgs/{username}")]
         public IActionResult PostMessages(string username)
         {
-            return null;
+            //Har taget udgangspunkt i at Helge IKKE validerer brugeren
+            var request = HttpContext.Request; 
+            string content = request.Form["content"]; //er ikke sikker på at jeg tror på denne 100%
+
+            Console.WriteLine(content);
+
+            // var user = _context.Users.Where(u => u.UserName == username).Select(u => u).FirstOrDefault();
+            // if (user == null) return NotFound("yeeeeet"); //Helge kigger ikke på om brugeren findes, lol - måske skal vi heller ikke
+
+            // var message = new Message
+            // {
+            //     User = user,
+            //     UserId = user.UserId,
+            //     Text = content,
+            //     PubDate = DateTime.UtcNow,
+            //     Flagged = 0
+            // }; 
+
+            // _context.Messages.Add(message);
+
+            return NoContent();
         }
 
         [HttpPost("{username}")]
@@ -50,14 +83,14 @@ namespace MiniTwit_Public_API.Controllers
             return null;
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("fllws/{username}")]
         [Route("fllws/{username}")]
         public IActionResult FollowUser(string username, int no = 100)
         {
             return null;
         }
 
-        [HttpPost("{username}")]
+        [HttpPost("fllws/{username}")]
         [Route("fllws/{username}")]
         public IActionResult ToggleFollowUser(string username)
         {
