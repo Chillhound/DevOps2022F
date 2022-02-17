30fcb6968502b112a92afe69980033062019e3d8
kaeppen <seho@itu.dk>
2022-02-17 15:04:25 +0100

    make tests runnable

--- backend/MiniTwit-Public-API/minitwit_sim_test.py
+++ backend/MiniTwit-Public-API/minitwit_sim_test.py
@@ -29,6 +29,7 @@ def init_db():
 os.system(f'rm {DATABASE}')
 init_db()
 
+# We've given all request verify=False to accomodate a missing certificate and not throw ssl exceptions
 
 def test_latest():
     # post something to updaet LATEST
@@ -36,12 +37,12 @@ def test_latest():
     data = {'username': 'test', 'email': 'test@test', 'pwd': 'foo'}
     params = {'latest': 1337}
     response = requests.post(url, data=json.dumps(data),
-                             params=params, headers=HEADERS)
+                             params=params, headers=HEADERS, verify =False)
     assert response.ok
 
     # verify that latest was updated
     url = f'{BASE_URL}/latest'
-    response = requests.get(url, headers=HEADERS)
+    response = requests.get(url, headers=HEADERS, verify =False)
     assert response.ok
     assert response.json()['latest'] == 1337
 
@@ -53,12 +54,12 @@ def test_register():
     data = {'username': username, 'email': email, 'pwd': pwd}
     params = {'latest': 1}
     response = requests.post(f'{BASE_URL}/register',
-                             data=json.dumps(data), headers=HEADERS, params=params)
+                             data=json.dumps(data), headers=HEADERS, params=params, verify =False)
     assert response.ok
     # TODO: add another assertion that it is really there
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 1
 
 
@@ -68,11 +69,11 @@ def test_create_msg():
     url = f'{BASE_URL}/msgs/{username}'
     params = {'latest': 2}
     response = requests.post(url, data=json.dumps(data),
-                             headers=HEADERS, params=params)
+                             headers=HEADERS, params=params, verify =False)
     assert response.ok
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 2
 
 
@@ -81,7 +82,7 @@ def test_get_latest_user_msgs():
 
     query = {'no': 20, 'latest': 3}
     url = f'{BASE_URL}/msgs/{username}'
-    response = requests.get(url, headers=HEADERS, params=query)
+    response = requests.get(url, headers=HEADERS, params=query, verify =False)
     assert response.status_code == 200
 
     got_it_earlier = False
@@ -92,7 +93,7 @@ def test_get_latest_user_msgs():
     assert got_it_earlier
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 3
 
 
@@ -100,7 +101,7 @@ def test_get_latest_msgs():
     username = 'a'
     query = {'no': 20, 'latest': 4}
     url = f'{BASE_URL}/msgs'
-    response = requests.get(url, headers=HEADERS, params=query)
+    response = requests.get(url, headers=HEADERS, params=query, verify =False)
     assert response.status_code == 200
 
     got_it_earlier = False
@@ -111,7 +112,7 @@ def test_get_latest_msgs():
     assert got_it_earlier
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 4
 
 
@@ -122,12 +123,12 @@ def test_register_b():
     data = {'username': username, 'email': email, 'pwd': pwd}
     params = {'latest': 5}
     response = requests.post(f'{BASE_URL}/register', data=json.dumps(data),
-                             headers=HEADERS, params=params)
+                             headers=HEADERS, params=params, verify =False)
     assert response.ok
     # TODO: add another assertion that it is really there
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 5
 
 
@@ -138,11 +139,11 @@ def test_register_c():
     data = {'username': username, 'email': email, 'pwd': pwd}
     params = {'latest': 6}
     response = requests.post(f'{BASE_URL}/register', data=json.dumps(data),
-                             headers=HEADERS, params=params)
+                             headers=HEADERS, params=params, verify =False)
     assert response.ok
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 6
 
 
@@ -152,17 +153,17 @@ def test_follow_user():
     data = {'follow': 'b'}
     params = {'latest': 7}
     response = requests.post(url, data=json.dumps(data),
-                             headers=HEADERS, params=params)
+                             headers=HEADERS, params=params, verify =False)
     assert response.ok
 
     data = {'follow': 'c'}
     params = {'latest': 8}
     response = requests.post(url, data=json.dumps(data),
-                             headers=HEADERS, params=params)
+                             headers=HEADERS, params=params, verify =False)
     assert response.ok
 
     query = {'no': 20, 'latest': 9}
-    response = requests.get(url, headers=HEADERS, params=query)
+    response = requests.get(url, headers=HEADERS, params=query, verify =False)
     assert response.ok
 
     json_data = response.json()
@@ -170,7 +171,7 @@ def test_follow_user():
     assert "c" in json_data["follows"]
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 9
 
 
@@ -182,15 +183,15 @@ def test_a_unfollows_b():
     data = {'unfollow': 'b'}
     params = {'latest': 10}
     response = requests.post(url, data=json.dumps(data),
-                             headers=HEADERS, params=params)
+                             headers=HEADERS, params=params, verify =False)
     assert response.ok
 
     # then verify that b is no longer in follows list
     query = {'no': 20, 'latest': 11}
-    response = requests.get(url, params=query, headers=HEADERS)
+    response = requests.get(url, params=query, headers=HEADERS, verify =False)
     assert response.ok
     assert 'b' not in response.json()['follows']
 
     # verify that latest was updated
-    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
+    response = requests.get(f'{BASE_URL}/latest', headers=HEADERS, verify =False)
     assert response.json()['latest'] == 11
