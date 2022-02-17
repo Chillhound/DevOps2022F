c20f617d119a9af506f9c75d13f0ef28d322309e
kaeppen <seho@itu.dk>
2022-02-17 15:00:32 +0100

    rename property

--- backend/Domain/DTO/CreateUserDTO.cs
+++ backend/Domain/DTO/CreateUserDTO.cs
@@ -8,7 +8,7 @@ namespace Domain.DTO
 {
     public class CreateUserDTO
     {
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
